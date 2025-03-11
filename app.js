955642c2c7196c87ad7af72179219dec6ced5461
Alberto Balderas <alberic@live.com.mx>
2025-03-10 21:49:57 -0600

    Update app.js
    Función limpiar y Función agregarAmigo (parcial)

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ let listaAmigoSorteado = [];
 // Función agregar amigo secreto
 function agregarAmigo(){
     let amigoSecreto = String(document.getElementById('amigo').value);
+    if(amigoSecreto === ""){
+        alert("Por favor, inserte un nombre.");
+        return;
+    }else {   
+    }   if(listaAmigoSorteado.includes(amigoSecreto)){
+            alert("Ya agregaste este amigo, agrega un nombre diferente");
+        }else {
+            listaAmigoSorteado.push(amigoSecreto);
+            
+    }
     console.log(`${amigoSecreto}`);
+    console.log(listaAmigoSorteado.length);
+    console.log(listaAmigoSorteado);
     limpiarCaja();
 }
 
