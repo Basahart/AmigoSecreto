28e6f27e59a36bea0540cdafe3e08f0fcac7c78d
Alberto Balderas <alberic@live.com.mx>
2025-03-10 21:04:03 -0600

    Update app.js
    Se agregan funciones básicas para validar función de botones

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Variables
+let listaAmigoSorteado = [];
+
+
+//Funciones
+
+// Función agregar amigo secreto
+function agregarAmigo(){
+    let amigoSecreto = String(document.getElementById('amigo').value);
+    console.log(`${amigoSecreto}`);
+    limpiarCaja();
+}
+
+// Función Sortear amigo secreto
+function sortearAmigo(){
+    console.log(`Sortear`);
+}
+
+//Función limpiar caja
+function limpiarCaja(){
+    document.getElementById('amigo').value = "";
+    document.getElementById('amigo').focus();
+}
+
